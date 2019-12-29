9431a2477b510d8c49154183330098a619644769
Misha <mvucikevik@gmail.com>
2019-12-29 21:15:22 +0100

    :white_check_mark:  Added unit-tests for NegotiationTabs

--- test/unit/specs/NegotiationTabs.spec.js
+++ test/unit/specs/NegotiationTabs.spec.js
@@ -1,11 +1,57 @@
 import Vue from 'vue'
-import NegotiationTabs from '@/components/NegotiationTabs'
+import NegotiationTabs from '@/components/NegotiationTabs.vue'
+import  { isUndefinedOrZero, getCurrentLondonWeatherData }  from "@/shared/mixins";
 
 describe('NegotiationTabs.vue', () => {
-  it('should render correct contents', () => {
+  
+  it('sets the correct default data', () => {
+    const defaultData = NegotiationTabs.data()
+    expect(defaultData.isEmployerTab).to.equal(true)
+    expect(defaultData.employerProposal).to.equal(0)
+    expect(defaultData.employeeProposal).to.equal(0)
+    expect(defaultData.currentTemp).to.equal(0)
+  })
+
+  it('verifies that updateProposedValue properly updates proposed value on employers tab', () => {
+    // 1. Arrange
+    const givenVal = 5;
     const Constructor = Vue.extend(NegotiationTabs)
     const vm = new Constructor().$mount()
-    // expect(vm.$el.querySelector('.hello h1').textContent)
-    //   .to.equal('Welcome to Your Vue.js App')
+    vm.isEmployerTab = true
+   // 2. Act
+
+    vm.updateProposedValue(givenVal)
+   // 3. Assert
+    expect(vm.employerProposal).to.equal(givenVal)
+    expect(vm.employeeProposal).to.equal(0)
   })
+
+  it('verifies that updateProposedValue properly updates proposed value on employee tab', () => {
+    // 1. Arrange
+    const givenVal = 5;
+    const Constructor = Vue.extend(NegotiationTabs)
+    const vm = new Constructor().$mount()
+    vm.isEmployerTab = false
+   // 2. Act
+    vm.updateProposedValue(givenVal)
+   // 3. Assert
+    expect(vm.employerProposal).to.equal(0)
+    expect(vm.employeeProposal).to.equal(givenVal)
+  })
+
+  it('verifies that resetNegotiation properly resets the proposed values', () => {
+    // 1. Arrange
+    const Constructor = Vue.extend(NegotiationTabs)
+    const vm = new Constructor().$mount()
+
+    vm.employerProposal = 12
+    vm.employeeProposal = 5
+   // 2. Act
+    vm.resetNegotiation()
+   // 3. Assert
+    expect(vm.employerProposal).to.equal(0)
+    expect(vm.employeeProposal).to.equal(0)
+  })
+
+  // TO DO: Test the modal ( show ) and the current weather call
 })
