aed915c189b1c77e0e549ba0766ddf188436bf86
Misha <mvucikevik@gmail.com>
2020-01-01 21:46:51 +0100

    Updated unit-tests

--- test/unit/specs/NegotiationTab.spec.js
+++ test/unit/specs/NegotiationTab.spec.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
 import NegotiationTab from '@/components/NegotiationTab'
+import { mount } from "@vue/test-utils"
 
 describe('NegotiationTab.vue', () => {
   it('should render correct contents', () => {
+    // 1. Arrange
+    const textContent = 'Submit'
     const Constructor = Vue.extend(NegotiationTab)
-    const vm = new Constructor().$mount()
-    // expect(vm.$el.querySelector('.hello h1').textContent)
-    //   .to.equal('Welcome to Your Vue.js App')
+    // 2. Act
+    const vm = new Constructor({propsData: { isInputShown: true }}).$mount()
+    
+    // 3. Assert
+    expect(vm.$el.querySelector('.entry-form button').textContent)
+      .to.equal(textContent)
+  })
+
+  it('should render execute submit properly', () => {
+    // 1. Arrange
+    const wrapper = mount(NegotiationTab)
+    const expectedVal = 12 
+    wrapper.vm.$refs = {
+      proposal : {
+        value : expectedVal // only numerical
+      }
+    }
+    // 2. Act
+    wrapper.vm.submitInput();
+    // 3. Assert
+    expect(wrapper.emitted()['proposed-value'][0][0]).to.equal(expectedVal)
   })
 })
--- test/unit/specs/mixins.spec.js
+++ test/unit/specs/mixins.spec.js
@@ -17,17 +17,23 @@ describe('Mixins.js', () => {
   })
 
   it('should return true if the input is zero', () => {
+      // 1. Arrange
       const input = 0;
+      // 2.Act // 3. Assert
       expect (isUndefinedOrZero(input)).to.equal(true);
   });
 
   it('should return true if the input is undefined', () => {
+    // 1. Arrange
     const input = undefined;
+    // 2.Act // 3. Assert
     expect (isUndefinedOrZero(input)).to.equal(true);
   });
 
   it('should return false if the input is 5', () => {
+    // 1. Arrange
     const input = 5;
+    // 2.Act // 3. Assert
     expect (isUndefinedOrZero(input)).to.equal(false);
   });
   
